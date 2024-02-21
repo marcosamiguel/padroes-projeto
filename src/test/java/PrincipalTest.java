e11bf27f9a5be4281c6edece17bcbfa168ddc76b
marcosmiguel <marcos.a.miguel@gmail.com>
2024-02-21 13:07:27 -0300

    Padrões de Projeto

---
+++ src/test/java/PrincipalTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PrincipalTest {
+
+    @Test
+    void testar() {
+        assertEquals("1", "1");
+    }
+}
+
